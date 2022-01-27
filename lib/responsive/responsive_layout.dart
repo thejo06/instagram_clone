55016abc8e2f453a3ae598555d1d1c37b1766ba6
thejo06 <yoshuapurba06@gmail.com>
2022-01-27 10:03:07 +0700

    first-commit

---
+++ lib/responsive/responsive_layout.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+import 'package:instagram_clone/utils/dimensions.dart';
+
+class ResponsiveLayout extends StatelessWidget {
+  final Widget webScreenlayout;
+  final Widget mobileScreenLayout;
+  const ResponsiveLayout({
+    Key? key,
+    required this.webScreenlayout,
+    required this.mobileScreenLayout,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return LayoutBuilder(
+      builder: (context, constraints) {
+        if (constraints.maxWidth > webScreenSize) {
+          return webScreenlayout;
+        }
+        // mobile screen
+        return mobileScreenLayout;
+      },
+    );
+  }
+}
